ddf72a65052054f5770cb347a24d276b8d097356
briyao <briannayao@ctemc.org>
2019-01-09 14:24:30 -0500

    Finish wonka game code

--- wonka_game.py
+++ wonka_game.py
@@ -6,16 +6,8 @@ class room1:
     def __init__(self, master):
         self.master = master
         self.frame = tk.Frame(self.master)
-        self.newWindow = gameroom('inventroom.png','hi',self.master)
-        '''self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
-        self.geometry('800x800+0+0')
-        # the container is where we'll stack a bunch of frames
-        # on top of each other, then the one we want visible
-        # will be raised above the others
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)'''
+        self.newWindow = gameroom('inventroom.png','hi',self.frame)
+        self.frame.pack()
 
 def gameroom(img,displaytext,root):
     screenwidth = 800
@@ -31,11 +23,16 @@ def gameroom(img,displaytext,root):
     title_label.config(font=("Courier", 14))
     title_label.pack()
     canvas.create_window(screenwidth/2,30,window = title_label, anchor = N) 
-    
+
     room_img = Image.open(img)
     resizedimg = room_img.resize((500,500))
-    screenroom_img = ImageTk.PhotoImage(resizedimg)  
-    canvas.create_image(screenwidth/2, 75, anchor = N, image=screenroom_img)  
+    screenroom_img = ImageTk.PhotoImage(resizedimg)
+    displayimg = Label(image = screenroom_img)
+    displayimg.image = screenroom_img
+    canvas.create_window(screenwidth/2,75,window = displayimg, anchor = N)
+    '''self.screenroom_img = screenroom_img
+    canvas.create_image(screenwidth/2, 75, anchor = N, image=screenroom_img)'''
+
     #label_img = Label(root,image=screenimg)
     #label_img.place(x=150,y=50)
     output_label = Label(canvas, text=displaytext, fg = 'white', bg = screenbg,wraplength=500)
