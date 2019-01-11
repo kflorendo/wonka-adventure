45cae4bee141def40cc7fa5e4de05b10ee090899
briyao <briannayao@ctemc.org>
2019-01-11 13:24:26 -0500

    Update wonka_game display

--- wonka_game.py
+++ wonka_game.py
@@ -2,23 +2,30 @@ import tkinter as tk     # python 2
 from tkinter import *  
 from PIL import ImageTk,Image  
 
+output_text = ''
+canvas = ''
+enter_btn = ''
+#root.configure(background='black')
+        
 class room1:
     def __init__(self, master):
         self.master = master
         self.frame = tk.Frame(self.master)
-        self.newWindow = gameroom('inventroom.png','hi',self.frame)
+        gameroom('inventroom.png','hi',self.frame)
+        display('hey')
         self.frame.pack()
 
 def gameroom(img,displaytext,root):
+    global output_text
+    global canvas
+    global enter_btn
     screenwidth = 800
     screenheight = 800
     screenbg = 'black'
+    canvas = Canvas(root, bg = screenbg, width = screenwidth, height = screenheight)
+    canvas.pack()
     #root = Tk()
     #root.geometry('800x800+0+0')
-    
-    canvas = Canvas(root, bg = screenbg, width = screenwidth, height = screenheight) 
-    #root.configure(background='black')
-    canvas.pack()
     title_label = Label(canvas, text='INVENT ROOM', fg = 'white', bg = screenbg)
     title_label.config(font=("Courier", 14))
     title_label.pack()
@@ -35,17 +42,35 @@ def gameroom(img,displaytext,root):
 
     #label_img = Label(root,image=screenimg)
     #label_img.place(x=150,y=50)
-    output_label = Label(canvas, text=displaytext, fg = 'white', bg = screenbg,wraplength=500)
+    '''output_label = Label(canvas, text=displaytext, fg = 'white', bg = screenbg,wraplength=500)
     output_label.config(font=("Courier", 14))
-    output_label.pack()
-    canvas.create_window(screenwidth/2,screenheight-200,window = output_label, anchor = N)    
+    output_label.pack()'''
+    output_text = canvas.create_text((screenwidth/2,screenheight-200), anchor = N, fill = 'white', font =('Courier',14),text=displaytext)
+    #canvas.create_window(screenwidth/2,screenheight-200,window = output_label, anchor = N)
+    #output_text = canvas.create_window(screenwidth/2,screenheight-200,window = output_label, anchor = N)    
     
-    user_entry = Entry(canvas, bd = 10)
+    user_entry = Entry(canvas, relief = FLAT, bd = 10)
     user_entry.config(font=("Courier", 14))
-    canvas.create_window(screenwidth/2,screenheight-25,window = user_entry, anchor = S)
+    user_entry.pack()
+    canvas.create_window(screenwidth/2-50,screenheight-25,window = user_entry, anchor = S)
+    
+    enter_btn = Button(canvas, text = "ENTER", font = ('Courier',14),command = enter())
+    enter_btn.configure(bd = 5)
+    enter_btn.pack()
+    canvas.create_window(screenwidth/2+125,screenheight-25,window = enter_btn, anchor = S)
     canvas.update()
     
     #root.mainloop()
+
+def enter():
+    print 'button clicked'
+    
+def display(displaytext):
+    global output_text
+    global canvas
+    canvas.itemconfig(output_text,text=displaytext)
+    canvas.update()
+    
 
 def main(): 
     root = tk.Tk()
